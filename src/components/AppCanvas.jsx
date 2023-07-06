973ef0abdbda6606824aa3baf19242c8299d0f21
Andrew van Duivenbode <andrew@creativetransmissions.com>
2023-07-06 15:22:26 +0100

    WIP texture

--- src/components/AppCanvas.jsx
+++ src/components/AppCanvas.jsx
@@ -1,29 +1,59 @@
-import React, { useEffect, useMemo, useRef } from 'react';
-import { Canvas, useFrame, useLoader } from '@react-three/fiber';
-import { Plane, Html, Loader } from '@react-three/drei';
-import { TextureLoader } from 'three';
-
-function ImagePlane() {
-  const ipfsHash = 'Qm...'; // replace with your hash
-  const url = `https://gateway.pinata.cloud/ipfs/${ipfsHash}`;
-
-  const texture = useLoader(TextureLoader, url);
-  
-  const mesh = useRef();
-  useFrame(() => (mesh.current.rotation.y += 0.01));
-
-  return (
-    <Plane args={[5, 5]} ref={mesh}>
-      <meshBasicMaterial attach="material" map={texture} />
-    </Plane>
-  );
-}
+import React, { useState, useEffect, useMemo, useRef } from 'react';
+import { useSelector } from 'react-redux';
+import { Canvas } from '@react-three/fiber';
+import ImagePlane from './ImagePlane';
 
 function AppCanvas() {
-  return (
-    <Canvas style={{ background: 'black', width: "100vw", height: "100vh"  }}>
-    </Canvas>
-  );
+
+    const parseNFTs = (ownedNfts) => {
+        return new Promise((resolve,reject)=>{
+            // Map each nftEntry to a Promise returned by parseNFT
+            const promises = ownedNfts.map(parseNFT);
+
+            Promise.allSettled(promises)
+            .then(results => {
+                const successfulPromises = results.filter(result => result.status === 'fulfilled' && result.value !== false);
+                resolve(successfulPromises.map(promise => promise.value));
+            })
+            .catch(error => {
+              console.error('Error processing promises:', error);
+            });            
+        })
+    
+    }
+      
+    const parseNFT = async (metaData) =>{
+    
+      if(metaData.image){
+        let imgUrl = (metaData.image.cachedUrl)?metaData.image.cachedUrl:(metaData.image.originalUrl)?metaData.image.originalUrl:null;
+          if(imgUrl){
+            return imgUrl;
+          }
+      }
+      return false;
+    }
+
+    const [imageUrls, setImageUrls] = useState([]);
+
+    let nftList = useSelector((state) => state.data.nftList);
+
+    useEffect(() => {
+        if (nftList) {
+          parseNFTs(nftList.ownedNfts).then((urls) => {
+            setImageUrls(urls);
+          });
+        }
+      }, [nftList]); // Only re-run the effect if nftList changes
+      
+
+    return (
+        <div>
+        <Canvas style={{ background: 'black', width: "100vw", height: "100vh"  }}>
+            {imageUrls.length > 0 && <ImagePlane key={0} imageUrl={imageUrls[0]} />}
+        </Canvas>
+        {imageUrls.length > 0 && <img src={'https://3desocial.com/api/image-proxy?url='+imageUrls[0]} alt="preview" />} 
+        </div>
+        )
 }
 
 export default AppCanvas;
--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -10,16 +10,17 @@ function HomePage() {
   const setData = (data) => {
     dispatch({
       type: 'SET_DATA',
-      data: data  // Your JSON data here
+      data: data
     });
   };
 
   const account = useAccount({
     onConnect({ address, connector, isReconnected }) {
-      setData({address:address});
-      fetchNFTs(address).then((nftList)=>{
 
-        parseNFTs(nftList.ownedNfts)
+      fetchNFTs(address).then((nftList)=>{
+        setData({address:address,
+                  nftList:nftList});        
+
       })
 
     },
@@ -44,50 +45,8 @@ function HomePage() {
   const constructNFTURL =() =>{
 
     const alchemyKey = process.env.REACT_APP_ALCHEMY_KEY;
-    console.log('REACT_APP_ALCHEMY_KEY: ',alchemyKey);
-    const owner = '0x154B4045F07B48C3B75D73a3f6C7C11Dfec95b4a';
+    const owner = process.env.REACT_APP_TEST_WALLET_ADDRESS;
     return 'https://eth-mainnet.g.alchemy.com/nft/v3/'+alchemyKey+'/getNFTsForOwner?owner='+owner+'&withMetadata=true&pageSize=100'    
-  }
-
-  const parseNFTs = (ownedNfts) => {
-    console.log('parseNFTs: ',ownedNfts);
-      return new Promise((resolve,reject)=>{
-          // Map each nftEntry to a Promise returned by parseNFT
-          const promises = ownedNfts.map(parseNFT);
-      
-         
-          
-      })
-  
-  }
-    
-  const parseNFT = async (metaData) =>{
-
-    if(metaData.image){
-        console.log('image: ',metaData.image.cachedUrl);
-        //return fetchImage(metaData);
-    }
-    return false;
-}
-
-const fetchImage = async (metaData) => {
-    let url = metaData.image;
-
-    return new Promise((resolve, reject) => {
-        const img = new Image();
-        img.onload = function() {
-            let width = this.width;
-            let height = this.height;
-            let imageData = { dims:{width:width,height:height}, img:this };
-            metaData.imageData = imageData;
-            resolve(metaData);
-        }
-
-        img.onerror = function() {
-            reject(new Error(`Failed to load image's URL: ${url}`));
-        }
-        img.src = url;
-    });
   }
 
   const { address, isConnecting, isDisconnected } = useAccount()
